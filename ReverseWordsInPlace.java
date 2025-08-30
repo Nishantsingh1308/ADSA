7b6a888e6c987a36f288f465c9fd6e808215b8aa
Nishantsingh1308 <167596306+Nishantsingh1308@users.noreply.github.com>
2025-08-31 03:00:46 +0530

    Add files via upload

---
+++ ReverseWordsInPlace.java
@@ -0,0 +1,34 @@
+public class ReverseWordsInPlace {
+    private static void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static String reverseWords(String str) {
+        char[] arr = str.toCharArray();
+        int n = arr.length;
+
+        reverse(arr, 0, n - 1);
+
+        int start = 0;
+        for (int end = 0; end <= n; end++) {
+            if (end == n || arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
+            }
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        String input = "Welcome To India";
+        System.out.println("Original: " + input);
+        System.out.println("Reversed: " + reverseWords(input));
+    }
+}
